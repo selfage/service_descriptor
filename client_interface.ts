885d3a76069c259286142d08bb5fc073406a40e2
Yuxuan Xie <teststaybaka@gmail.com>
2024-08-20 15:57:03 -0700

    Add NodeClientOptions. Release

--- client_interface.ts
+++ client_interface.ts
@@ -8,6 +8,11 @@ export interface WebClientInterface {
   send(request: any, options?: WebClientOptions): Promise<any>;
 }
 
+export interface NodeClientOptions {
+  timeout?: number; // ms
+  retries?: number; // Number of retries when failure is not due to timeout.
+}
+
 export interface NodeClientInterface {
-  send(request: any): Promise<any>;
+  send(request: any, options?: NodeClientOptions): Promise<any>;
 }
